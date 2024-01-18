54bf309c548052704f8f24018d4854692904cfd6
Dariga <dariga.syzdykova05@gmail.com>
2024-01-18 18:33:47 +0600

    First commit

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import Image from 'react'
+import Link from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col
+    text-black-100 mt-5 border-t 
+    border-gray-100">
+        <div className='flex max-md:flex-col'>
+
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/index.ts
@@ -0,0 +1,13 @@
+import Hero from '../components/Hero'
+import CustomButton from '../components/CustomButton'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
